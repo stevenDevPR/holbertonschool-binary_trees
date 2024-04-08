5bdcf1c2aaf8fc381d7eb27d9b65369f93e84542
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-08 13:22:59 -0400

    adding header sata structures for print function

--- binary_trees.h
+++ binary_trees.h
@@ -37,5 +37,11 @@ typedef struct binary_tree_s avl_t;
  */
 typedef struct binary_tree_s heap_t;
 
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+
+static size_t _height(const binary_tree_t *tree);
+
+void binary_tree_print(const binary_tree_t *tree);
+
 
 #endif
