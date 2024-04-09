a6b2a9c8ef7501200be0383472b71256220cb3c0
Abimael <icemask2612@gmail.com>
2024-04-09 13:36:15 -0400

    fixed betty and gcc

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,6 +8,7 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+
 size_t left_size, right_size;
 
 if (tree == NULL)
@@ -15,6 +16,5 @@ if (tree == NULL)
 
 left_size = binary_tree_size(tree->left);
 right_size = binary_tree_size(tree->right);
-
-return ((left_size + right_size) + 1);
+return ((left_size + right_size) +1);
 }
