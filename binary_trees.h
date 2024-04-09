3e94e1aaffff4d3fc7320c967e45b01213fbec07
stevendevpr <stevenbonilla46@yahoo.com>
2024-04-09 11:05:58 -0400

    pushing header edits

--- binary_trees.h
+++ binary_trees.h
@@ -36,8 +36,14 @@ typedef struct binary_tree_s avl_t;
  */
 typedef struct binary_tree_s heap_t;
 
+/**
+ * tasks 0-7
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/*
+ * Prototype for print function
+ */
 void binary_tree_print(const binary_tree_t *tree);
 
 /**
