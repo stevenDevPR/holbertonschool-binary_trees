0a363aaf2aa16cb2c1c4a90b372ad14fe8f17719
luissoto7710 <luissoto7710@gmail.com>
2024-04-10 11:34:49 -0400

    My Commit

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,12 +8,12 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 if (node == NULL || node->parent == NULL)
-return NULL;
+return (NULL);
 
 if (node->parent->left == node)
-return node->parent->right;
+return (node->parent->right);
 else
-return node->parent->left;
+return (node->parent->left);
 }
 
 /**
