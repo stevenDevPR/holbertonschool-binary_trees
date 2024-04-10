57f73d89e5820a62bd6851c59a7fc552257b8e5a
luissoto7710 <luissoto7710@gmail.com>
2024-04-10 06:12:17 -0400

    fixxed betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,10 +9,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 if (tree == NULL)
 return (0);
- 
+
 if (tree->left == NULL && tree->right == NULL)
 return (1);
-    
+
 
 if (tree->left != NULL && tree->right != NULL)
 return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
